df05015726763b7ea6287bc9fce64f3406c80755
Arbaz <arbaz582021@gmail.com>
2022-06-13 16:21:38 +0500

    add

--- card-system/src/app/Mainportal/view-card/view-card.component.ts
+++ card-system/src/app/Mainportal/view-card/view-card.component.ts
@@ -1,5 +1,7 @@
+import { ResourceLoader } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { MessengerService } from 'src/app/sharedportal/messenger.service';
+import { ServicesService } from 'src/app/sharedportal/services.service';
 
 @Component({
   selector: 'app-view-card',
@@ -8,8 +10,12 @@ import { MessengerService } from 'src/app/sharedportal/messenger.service';
 })
 export class ViewCardComponent implements OnInit {
   Data:any;
+  SerivceData:any=[];
+  FilteredArray:any = [];
+
   constructor(
-    private _MessengerSevices:MessengerService
+    private _MessengerSevices:MessengerService,
+    private _MyServiceData:ServicesService
   ) { }
 
   ngOnInit(): void {
@@ -17,7 +23,18 @@ export class ViewCardComponent implements OnInit {
       this.Data=DataComingFromMyMessengerService;
       // yahan hum is liya Data ko nahi initialized kar sakta q ka observable ma data bohut sara a raha hota ha
     }) 
-    console.log(this.Data)
+    // console.log(this.Data)
+    this.DataComingFromMyService()
+   }
+
+   DataComingFromMyService(){
+     this.SerivceData=this._MyServiceData.getdata()
+    //  console.log(this.SerivceData);
+     this.FilteredArray=this.SerivceData.filter((Result:any)=>{return Result.id==this.Data});
+     console.log(this.FilteredArray)
+     
+     
+
    }
 
 }
